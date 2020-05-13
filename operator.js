daff78d41835777f2f027a8b8e6da9d97578be68
tbmg99 <rajs04011@gmail.com>
2020-05-13 11:33:19 -0700

    h2 adding Hello.

---
+++ operator.js
@@ -0,0 +1,12 @@
+var year, yearDonald, yearSam;
+now = 2020;
+ageDonald = 26;
+ageSam= 30;
+//Math operators
+yeaDonald = now - ageDonald;
+yearSam = now - ageSam;
+
+console.log(yearSam);
+console.log (now + 2);
+console.log (now * 4);
+console.log( now/30);
---
+++ variable.js
@@ -0,0 +1,17 @@
+// Variable (var) is a container or memory where we can store a  value and reuse again and again. Here variable container name is firstName where we assigned or store a value string 'John';
+
+// var year, yearJohn, yearmark;
+// now = 2018;
+// ageJohn = 28;
+// ageMark = 33;
+// //Math Operators
+// yearJohn = now - ageJohn;
+// yearmark = now - ageMark;
+// console.log(yearJohn); 
+ 
+// console.log ( now + 2);
+// console.log (now *2);
+// console.log(now / 10);
+//  //Logical operators
+//  var johnOlder = ageJohn < ageMark;
+//  console.log(johnOlder);
